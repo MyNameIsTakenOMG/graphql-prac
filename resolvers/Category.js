369d492026b01cd27c54f072954514bf7d2bdf22
Sean Fang <fangzhengonly@gmail.com>
2023-07-07 15:40:28 -0400

    add average filter to resolver Category

--- resolvers/Category.js
+++ resolvers/Category.js
@@ -4,14 +4,30 @@ const Category = {
     const products = context.products;
     const { id } = parent;
     const filter = args.filter;
+    const { reviews } = context;
     let filteredProducts = products.filter(
       (product) => product.category_id === id
     );
     if (filter) {
-      if (filter.onSale === true) {
-        filteredProducts = filteredProducts.filter(
-          (product) => product.onSale === true
-        );
+      const { onSale, avgRating } = filter;
+      if (onSale) {
+        filteredProducts = filteredProducts.filter((product) => product.onSale);
+      }
+
+      if ([1, 2, 3, 4, 5].includes(avgRating)) {
+        filteredProducts = filteredProducts.filter((product) => {
+          // get reviews for the product
+          let sumRating = 0;
+          let nums = 0;
+          reviews.forEach((review) => {
+            if (review.productId === product.id) {
+              sumRating += review.rating;
+              nums += 1;
+            }
+          });
+          const avgProductRating = sumRating / nums;
+          return avgProductRating >= avgRating;
+        });
       }
     }
     return filteredProducts;
