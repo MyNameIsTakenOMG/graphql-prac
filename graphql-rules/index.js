b0d655e19b0803c5fd1564709c6ce69a1f9a0168
Sean Fang <fangzhengonly@gmail.com>
2023-07-08 12:00:21 -0400

    finish creating schema

--- graphql-rules/index.js
+++ graphql-rules/index.js
@@ -6,29 +6,79 @@ exports.typeDefs = `
     cars: [Car!]!
   }
 
+  type Mutation{
+    groupDelete(groudId: ID!)
+    groupUnpublish(groudId: ID!)
+    groupPublish(groudId: ID!)
+    groupCreate(
+      groupInput: GroupInput!
+    )
+    groupUpdate(
+      groupId: ID!
+      groupInput: GroupInput!,
+    ) : GroupUpdatePayload 
+    groupAddCars(groudId: ID!, carId: ID!)
+    groupRemoveCars(groudId: ID!, carId: ID!)
+  }
+
+  type GroupUpdatePayload{
+    group: Group
+    userErrors: [UserErrors!]!
+  }
+  type UserErrors{
+    message: String!
+    field: [String!]!
+  }
+
+  input GroupInput{
+    name: String
+    image: ImageInput
+    description: String
+    featureSet: GroupFeatureSet
+  }
+
+  input ImageInput{
+    url: String!
+  }
+
   type Car {
     id: ID!
     color: String!
     make: String!
   }
 
-  type MannualGroup {
-    Image
-    [GroupMembership]
-  }
-  type AutomaticGroup {
-    Image
-    [GroupMembership]
-    [AutomaticallyGroupFeatures]
-  }
-
-  type AutomaticGroupFeatures{
+  type Group {
+    id: ID!
+    featureSet: GroupFeatureSet
+    hasCar(id: ID!): Boolean!
+    cars(skip: Int!, take: Int!): [Car!]!
+    name: String!
+    image: Image!
+    description: String!
   }
 
-  type GroupMembership{
-    Group
-    Car
+  type Image{
+    id: ID!
+    url: String!
   }
+
+  type GroupFeatureSet {
+    features: [GroupFeatures!]!
+    applyFeaturesSeperately: Boolean!
+  }
+
+  type GroupFeatures{
+    feature: GroupFeatureFields!
+  }
+
+  enum GroupFeatureFields{
+    INCLINE_ENGINE
+    FOUR_CYLINDER_ENGINE
+    TWIN_CYLINDER_ENGINE
+    RED_PAINT
+    BLACK_PAINT
+  }
+
 `;
 
 const server = new ApolloServer({
@@ -51,3 +101,11 @@ const start = async () => {
 };
 
 start();
+
+// --------------------------------------------------------
+// declaration accepts: | null | []   | [null] | [{foo: 'BAR'}]
+// ------------------------------------------------------------------------
+// [Vote!]!             | no   | yes  | no     | yes
+// [Vote]!              | no   | yes  | yes    | yes
+// [Vote!]              | yes  | yes  | no     | yes
+// [Vote]               | yes  | yes  | yes    | yes
